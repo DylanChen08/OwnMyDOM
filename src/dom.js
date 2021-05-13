db9fbf4b8e989735660661346cff04cd9439a7e2
DC <easetreychen@gmail.com>
2021-05-13 23:04:15 +0800

    update compare

--- src/dom.js
+++ src/dom.js
@@ -1,104 +1,139 @@
-//dom.js
-
 window.dom = {
-    create(element) {
-        const container = document.createElement('template')
-        container.innerHTML = element.trim()
-        console.log(container)
-        return container.content.firstChild
+    create(string) {
+      const container = document.createElement("template");
+      container.innerHTML = string.trim();
+      return container.content.firstChild;
     },
-    after(previousNode, afterNode) {
-        previousNode.parentNode.insertBefore(afterNode, previousNode.nextSiblings)
+    after(node, node2) {
+      node.parentNode.insertBefore(node2, node.nextSibling);
     },
-    before(previousNode, afterNode) {
-        previousNode.parentNode.insertBefore(afterNode, previousNode)
+    before(node, node2){
+      node.parentNode.insertBefore(node2, node);
     },
-    append(parent, node) {
-        parent.appendChild(node)
+    append(parent, node){
+      parent.appendChild(node)
     },
-    wrap(node, parent) {
-        dom.before(node, parent)
-        dom.append(parent, node)
+    wrap(node, parent){
+      dom.before(node, parent)
+      dom.append(parent, node)
     },
-    remove(node) {
-        node.parentNode.removeChild(node)
-        return node
+    remove(node){
+      node.parentNode.removeChild(node)
+      return node
     },
-    empty(node) {
-        // const {childNodes} = node // 相当于 const childNodes = node.childNodes
-        const array = []
-        let elementExist = node.firstChild
-        while (elementExist) {
-            array.push(dom.remove(node.firstChild))
-            elementExist = node.firstChild
+    empty(node){
+      const array = []
+      let x = node.firstChild
+      while(x){
+        array.push(dom.remove(node.firstChild))
+        x = node.firstChild
+      }
+      return array
+    },
+    attr(node, name, value){ // 重载
+      if(arguments.length === 3){
+        node.setAttribute(name, value)
+      }else if(arguments.length === 2){
+        return node.getAttribute(name)
+      }
+    },
+    text(node, string){ // 适配
+      if(arguments.length ===2 ){
+        if('innerText' in node){
+          node.innerText = string 
+        }else{
+          node.textContent = string 
         }
-        return array
-    },
-    attr(node, name, value) {//重载
-        if (arguments.length === 3) {
-            return node.setAttribute(name, value)
-        } else if (arguments.length === 2) {
-            return node.getAttribute(name)
+      }else if(arguments.length === 1){
+        if('innerText' in node){
+          return node.innerText
+        }else{
+          return node.textContent
         }
+      }
     },
-    text(node, string) {
-        if (arguments.length === 2) {
-            if ('innerText' in node) {
-                node.innerText = string //兼容IE
-            } else {
-                node.textContent = string //Chorome，EDGE 主流浏览器
-            }
-        } else if (arguments.length === 1) {
-            if ('innerText' in node) {
-                return node.innerText
-            } else { return node.textContent }
-
-        }
-
+    html(node, string){
+      if(arguments.length === 2){
+        node.innerHTML = string
+      }else if(arguments.length === 1){
+        return node.innerHTML 
+      }
     },
-    html(node, string) {
-        if (arguments.length === 2) {
-            node.innerHTML = string
-        } else if (arguments.length === 1) {
-            return node.innerHTML
+    style(node, name, value){
+      if(arguments.length===3){
+        // dom.style(div, 'color', 'red')
+        node.style[name] = value
+      }else if(arguments.length===2){
+        if(typeof name === 'string'){
+          // dom.style(div, 'color')
+          return node.style[name]
+        }else if(name instanceof Object){
+          // dom.style(div, {color: 'red'})
+          const object = name
+          for(let key in object){
+            node.style[key] = object[key]
+          }
         }
-    },
-    style(node, name, value) {
-        if (arguments.length === 3) {
-            node.style[name] = value
-        } else if (arguments.length === 2) {
-            if (typeof name === 'string') {
-                return node.style[name]
-            } else if (name instanceof Object) {
-                const object = name
-                for (let key in object) {
-                    node.style[key] = object[key]
-                }
-            }
-        }
-
+      }
     },
     class: {
-        add(node, className) { node.classList.add(className) },
-        remove(node, className) { node.classList.remove(className) },
-        has(node, className) { return node.classList.contains(className) }
+      add(node, className){
+        node.classList.add(className)
+      },
+      remove(node, className){
+        node.classList.remove(className)
+      },
+      has(node, className){
+        return node.classList.contains(className)
+      }
     },
-    on(node,eventname,fn){
-        node.addEventListener(eventname,fn)
+    on(node, eventName, fn){
+      node.addEventListener(eventName, fn)
     },
-    off(node,eventname,fn){
-        node.removeEventListener(eventname,fn)
+    off(node, eventName, fn){
+      node.removeEventListener(eventName, fn)
     },
-    find(selector,scope){
-        return (scope||document).querySelector(selector)
+    find(selector, scope){
+      return (scope || document).querySelectorAll(selector)
     },
     parent(node){
-        return node.parentNode
+      return node.parentNode
     },
     children(node){
-        return node.children
+      return node.children
     },
     siblings(node){
-        return Array.from(node.parentNode.children).filter(n=>n!==node)
+      return Array.from(node.parentNode.children)
+      .filter(n=>n!==node)
+    },
+    next(node){
+      let x = node.nextSibling
+      while(x && x.nodeType === 3){
+        x = x.nextSibling
+      }
+      return x
+    },
+    previous(node){
+      let x = node.previousSibling
+      while(x && x.nodeType === 3){
+        x = x.previousSibling
+      }
+      return x
+    },
+    each(nodeList, fn){
+      for(let i=0;i<nodeList.length;i++){
+        fn.call(null, nodeList[i])
+      }
+    },
+    index(node){
+      const list = dom.children(node.parentNode)
+      let i
+      for(i=0;i<list.length;i++){
+        if(list[i] === node){
+          break
+        }
+      }
+      return i
     }
-}
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
-//index.js
+// document.createElement('div')
+const div = dom.create("<div>newDiv</div>");
+console.log(div);
 
-const div1 = dom.create(`<div>newdiv</div>`)
-console.log(div1)
-const test2 = dom.after(test, div1)
-console.log(`test2 is ${test2}`)
+dom.after(test, div);
 
-const div3 = dom.create('<div id="parents"></div>')
+const div3 = dom.create('<div id="parent"></div>')
+dom.wrap(test, div3)
+
 
 const nodes = dom.empty(window.empty)
 console.log(nodes)
 
-dom.attr(test, 'title', 'hi here')
+dom.attr(test, 'title', 'Hi, I am Frank')
 const title = dom.attr(test, 'title')
-console.log(`Title is '${title}' `)
+console.log(`title: ${title}`)
 
-dom.text(test, '你好，xxx999')
 
-dom.style(test, 'border', '1px solid red')
+dom.text(test, '你好，这是新的内容')
+dom.text(test)
+
+dom.style(test, {border: '1px solid red', color: 'blue'})
+console.log(dom.style(test, 'border'))
+dom.style(test, 'border', '1px solid black')
 
 dom.class.add(test, 'red')
 dom.class.add(test, 'blue')
 dom.class.remove(test, 'blue')
-console.log(dom.class.has(test, 'blue'))
+console.log(dom.class.has(test,'blue'))
 
-const fn = () => {
-    console.log('点击了')
+const fn = ()=>{
+  console.log('点击了')
 }
 dom.on(test, 'click', fn)
-// dom.off(test,'click',fn)
+dom.off(test, 'click', fn)
+
 
 const testDiv = dom.find('#test')[0]
-console.log(`testDiv is ${testDiv}`)
-// const test88 = dom.find('.red', testDiv)
-// console.log(`test88 is ${test88}`)
+console.log(testDiv)
+const test2 = dom.find('#test2')[0]
+console.log(dom.find('.red', test2)[0])
 
-console.log(`dom.find('#s2')[0] ${dom.find('#s2')[0]}`)
+console.log(dom.parent(test))
+
+const s2 = dom.find('#s2')[0]
+console.log(dom.siblings(s2))
+console.log(dom.next(s2))
+console.log(dom.previous(s2))
+
+const t = dom.find('#travel')[0]
+dom.each(dom.children(t), (n)=> dom.style(n, 'color', 'red'))
+
+console.log(dom.index(s2))
